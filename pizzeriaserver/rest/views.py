27faffdda818605259e8193bf44d96678ff50b36
Paul Estrada <PaulEstrada@MacBook-Pro-de-Paul-2.local>
2019-02-08 00:07:11 -0500

    fix

--- pizzeriaserver/rest/views.py
+++ pizzeriaserver/rest/views.py
@@ -770,7 +770,7 @@ def getCarrito(request):
             for combinacion in combinacion_combos:
                 combo = combinacion.combo
                 combos_ARR.append({
-                    "ID" : combinacion,
+                    "ID" : combinacion.id,
                     "NOMBRE" : combo.nombre,
                     "DESCRIPCION" : combo.descripcion,
                     "IMAGEN_URL" : IP + combo.img_url.url,
